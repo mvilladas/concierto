dff61dc64cb32ca2c54689a717148bdde9fb2833
mvilladas <mvilladas@unal.edu.com>
2022-06-07 18:21:17 -0500

    Primer comit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+echo "Hola mundo";
+$host= "localhost";
+$user = "root";
+$password ="";
+$link = mysqli_connect($host,$user,$password)or die ("Error en conexion");
+$dbname = "taquillabd";
+
+mysqli_select_db($link,$dbname)or die ( "Error al conectar en base de datos");

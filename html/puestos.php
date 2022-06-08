e4e0c4d5945b6d36bf04526000c684ed9ac50d86
mvilladas <mvilladas@unal.edu.com>
2022-06-08 01:17:35 -0500

    mejoritas aun

--- html/puestos.php
+++ html/puestos.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ingresar datos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-</head>
+    <link href="../css/style.css"  rel="stylesheet" type="text/css">
 </head>
 <body>
 <div class="bg-image"
@@ -16,19 +16,19 @@
         background-repeat: no-repeat;
         background-size: 100% auto;">
     <div class="container pt-5 px-5">
-        <div class="card">
+        <div class="card tarjeta">
             <form class="p-4">
                 <h2 class="m-2 my-4">Ingreso de datos</h2>
                 <div class="mb-3 row">
-                    <label  class="form-label col-sm-2">Puesto</label>
+                    <label  class="form-label col-sm-2">Evento</label>
                     <select class="form-select col" aria-label="Default select example">
                         <option selected>Escoge el evento que deseas</option>
                         <option value="1">Diamante sur</option>
                     </select>
                 </div>
                 <div class="mb-3 row">
-                    <label for="numero" class="form-label col-sm-2">Numero de entradas</label>
-                    <input type="number" class="form-control col" id="numero"  placeholder="Ingrese el número de silla">
+                    <label for="numero" class="form-label col-sm-2">Número de entradas</label>
+                    <input type="number" class="form-control col" id="numero"  placeholder="Ingrese el número entradas">
                 </div>
                 <div class="mb-3 row">
                     <label  class="ml-5 form-label col-sm-2  ">Localidad</label>
@@ -49,10 +49,8 @@
                     <label for="puesto" class="form-label col-sm-2">Puesto</label>
                     <input type="text" class="form-control col" id="puesto"  placeholder="Ingrese el número de silla">
                 </div>
-                <div class="d-grid gap-2">
-                    <div class="d-grid gap-2">
-                        <button type="button" class="btn btn-primary ">Enviar</button>
-                    </div>
+                <div class="d-grid gap-2 px-5">
+                    <button type="button" class="btn btn-primary ">Enviar</button>
                 </div>
             </form>
         </div>
